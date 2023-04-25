71444ea80dcb25ee59e8db4fdceeafb0218677f5
Biny19 <binalfew.tadele@gmail.com>
2023-04-25 01:58:39 -0700

    It's all about 0x1B. C - Sorting algorithms & Big O project in C programming

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,23 +11,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t j, i = size - 1;
-if (size < 2 || array == NULL)
-return;
-while (i > 0)
-{
-j = 0;
-while (j < i)
-{
-if (array[j] > array[j + 1])
-{
-array[j] ^= array[j + 1];
-array[j + 1] = array[j] ^ array[j + 1];
-array[j] ^= array[j + 1];
-print_array(array, size);
-}
-j++;
-}
-i--;
-}
+	size_t j, i = size - 1;
+	if (size < 2 || array == NULL)
+		return;
+	while (i > 0)
+	{
+		j = 0;
+		while (j < i)
+		{
+			if (array[j] > array[j + 1])
+			{
+				array[j] ^= array[j + 1];
+				array[j + 1] = array[j] ^ array[j + 1];
+				array[j] ^= array[j + 1];
+				print_array(array, size);
+			}
+			j++;
+		}
+		i--;
+	}
 }
