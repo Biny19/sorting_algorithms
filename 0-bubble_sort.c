c1c967ecbb88425d63a060bc9987630f9637e897
Biny19 <binalfew.tadele@gmail.com>
2023-04-25 01:34:41 -0700

    It's all about 0x1B. C - Sorting algorithms & Big O project in C programming

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,30 @@
 #include "sort.h"
 /**
- *bubble_sort - sorts an array of integers in ascending orde
- *@array: the array to be sort
- *@size: the size of the array
+ * bubble_sort - Bubble Sort Algorithm
+ * @array: array to sort
+ * @size: size of the array
+ * Description: A sort algorithm implemented using the bubble
+ * sort algorithm
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t j, i;
-int n;
-if (size < 2)
+size_t j, i = size - 1;
+if (size < 2 || array == NULL)
 return;
-for (i = 0; i < size - 1; i++)
+while (i > 0)
 {
-for (j = 0; j < size - i - 1; j++)
+j = 0;
+while (j < i)
 {
 if (array[j] > array[j + 1])
 {
-n = array[j];
-array[j] = array[j + 1];
-array[j + 1] = n;
+array[j] ^= array[j + 1];
+array[j + 1] = array[j] ^ array[j + 1];
+array[j] ^= array[j + 1];
 print_array(array, size);
 }
+j++;
 }
+i--;
 }
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,52 @@
 #include "sort.h"
 /**
- * bubble_sort - Bubble Sort Algorithm
- * @array: array to sort
- * @size: size of the array
- * Description: A sort algorithm implemented using the bubble
- * sort algorithm
+ * swap - swap 2 element in an list
+ * @head: head of list
+ * @a: node
+ * @b: node
  */
-void bubble_sort(int *array, size_t size)
+void swap(listint_t *a, listint_t *b, listint_t **head)
 {
-size_t j, i = size - 1;
-if (size < 2 || array == NULL)
+listint_t *aux1 = NULL, *aux2 = NULL;
+if (a == NULL || b == NULL)
 return;
-while (i > 0)
-{
-j = 0;
-while (j < i)
-{
-if (array[j] > array[j + 1])
-{
-array[j] ^= array[j + 1];
-array[j + 1] = array[j] ^ array[j + 1];
-array[j] ^= array[j + 1];
-print_array(array, size);
+aux1 = a->prev;
+aux2 = b->next;
+if (aux1)
+aux1->next = b;
+if (aux2)
+aux2->prev = a;
+a->next = aux2;
+a->prev = b;
+b->next = a;
+b->prev = aux1;
+if (aux1 == NULL)
+*head = b;
 }
-j++;
+/**
+ * insertion_sort_list  - insertion_sort_list
+ * @list: doubly liked list
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *head, *prev;
+int value;
+if (list == NULL || (*list)->next == NULL || (*list) == NULL)
+{
+return;
 }
-i--;
+head = *list;
+while (head)
+{
+prev = head->prev;
+value = head->n;
+while (prev && prev->n > value)
+{
+swap(prev, head, list);
+print_list(*list);
+prev = head->prev;
+}
+head = head->next;
 }
 }
