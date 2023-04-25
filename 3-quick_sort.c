37f708199b2243ec8dd94707ea7c92d8d28cffc9
Biny19 <binalfew.tadele@gmail.com>
2023-04-25 05:49:51 -0700

    It's all about sorting algorithm and Bgo

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * swap - swap two int
  * @a: int
@@ -7,10 +8,11 @@
  */
 void swap(int *a, int *b)
 {
-int tmp;
-tmp = *a;
-*a = *b;
-*b = tmp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 /**
  * partition - Partition an array and using pivot
@@ -22,21 +24,22 @@ tmp = *a;
  */
 int partition(int *array, int low, int high, size_t size)
 {
-int pivot = array[high];
-int x = low - 1, y;
-for (y = low; y <= high; y++)
-{
-if (array[y] <= pivot)
-{
-x++;
-if (x != y)
-{
-swap(&array[x], &array[y]);
-print_array(array, size);
-}
-}
-}
-return (x);
+	int pivot = array[high];
+	int x = low - 1, y;
+
+	for (y = low; y <= high; y++)
+	{
+		if (array[y] <= pivot)
+		{
+			x++;
+			if (x != y)
+			{
+				swap(&array[x], &array[y]);
+				print_array(array, size);
+			}
+		}
+	}
+	return (x);
 }
 /**
  * lomuto_qsort - Sorting Recursively an Array
@@ -48,13 +51,14 @@ return (x);
  */
 void lomuto_qsort(int *array, int low, int high, size_t size)
 {
-int i;
-if (low < high)
-{
-i = partition(array, low, high, size);
-lomuto_qsort(array, low, i - 1, size);
-lomuto_qsort(array, i + 1, high, size);
-}
+	int i;
+
+	if (low < high)
+	{
+		i = partition(array, low, high, size);
+		lomuto_qsort(array, low, i - 1, size);
+		lomuto_qsort(array, i + 1, high, size);
+	}
 }
 /**
  * quick_sort - Quick Sort Algorithme using lomuto partition
@@ -64,5 +68,5 @@ lomuto_qsort(array, i + 1, high, size);
  */
 void quick_sort(int *array, size_t size)
 {
-lomuto_qsort(array, 0, size - 1, size);
+	lomuto_qsort(array, 0, size - 1, size);
 }
